64dc89fdc4a75d4742c18ecff04be6030fa9f9b4
Nicole-Aurore <nicolemienandi@gmail.com>
2025-10-01 15:00:29 +0100

    Premier commit : app liste produits avec filtrage

--- script.js
+++ script.js
@@ -4,12 +4,29 @@ const produits = [
   { nom: "Casquette", prix: 15 }
 ];
 
-
 const liste = document.getElementById("maSuperListe");
 
+produits.forEach(produit => {
+  const li = document.createElement("li");
+  li.textContent = produit.nom + " - " + produit.prix + "€";
+  liste.appendChild(li);
+});
 
-produits.forEach(produit=>{
+document.getElementById("btnFiltre").addEventListener("click", () => {
+  const produitsFiltres = produits.filter(produit => produit.prix < 30);
+  liste.innerHTML = "";
+  produitsFiltres.forEach(produit => {
     const li = document.createElement("li");
-    li.textContent = produit.nom + " - " + produit.prix+ "€";
+    li.textContent = produit.nom + " - " + produit.prix + "€";
     liste.appendChild(li);
-})
+  });
+});
+
+document.getElementById("btnTous").addEventListener("click", () => {
+  liste.innerHTML = "";
+  produits.forEach(produit => {
+    const li = document.createElement("li");
+    li.textContent = produit.nom + " - " + produit.prix + "€";
+    liste.appendChild(li);
+  });
+});
